fbde41e499b5836dcb0354bc97cb7c9e15841f35
Wes Smith <wesleysmith@yahoo.com>
2023-12-31 13:35:44 -0800

    Now need to check crossovers

--- 2023/Day18/Program.cs
+++ 2023/Day18/Program.cs
@@ -6,11 +6,18 @@ using System.Diagnostics;
 using System.Text;
 using MoreLinq;
 using EmnumerableEx;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
 //using MoreLinq.Extensions;
 
 string NORMAL      = Console.IsOutputRedirected ? "" : "\x1b[39m";
 string RED         = Console.IsOutputRedirected ? "" : "\x1b[91m";
 string GREEN       = Console.IsOutputRedirected ? "" : "\x1b[92m";
+
+
+int RenderCount = 0;
+int RenderSkip = 450;
+int RenderThrow = RenderSkip + 5;
 
 bool sample = false;
 
@@ -29,145 +36,226 @@ Part1(instructions);
 Console.Out.WriteLine($"Finished in {sw.ElapsedMilliseconds}ms");
 
 
-void Part1(List<Instruction> instructions)
+void Part1(List<Instruction> instructionsX)
 {
+    
     
     long areaAdded = 0;
 
-    while (instructions.Count > 4) {
+    int mainLoopCount = 0;
 
+    var ill = new LinkedList<Instruction>(instructionsX);
         
+    while (ill.Count > 4) {
 
-         var bump = 
-             GetTurnList(instructions)
-            .Tripplewise((c1, c2, c3) => c1.ToString() + c2.ToString() + c3.ToString())
-            .Index(0)
-            .FirstOrDefault(kvp => kvp.Key > 0 && kvp.Value is "RLL" or "RRL", new KeyValuePair<int, string>(-1, ""));
-            
-        if (bump.Key >= 0) {
-            
-            Console.Write($"Found {bump.Value} Bump at index {bump.Key} ");
+        Console.WriteLine($"Looper {mainLoopCount}");
 
-            var bumpIndex = bump.Key;
+        if (mainLoopCount++ > 50) {
+            throw new Exception("Too many main loops");
+        }
+        var illCurrent = ill.First;
+        while (illCurrent != null) {
 
-           
+            var i0 = illCurrent;
+            var i1 = i0?.Next;
+            var i2 = i1?.Next;
+            var i3 = i2?.Next;
+            var i4 = i3?.Next;
 
-            if (bump.Value == "RLL") {
-                
-                // Example: If bump = 1, then bump is instructions 2,3,4 and 1 and 5 are pre and post segments.
-                
-                var pre = instructions[bumpIndex];
-                var s1 = instructions[bumpIndex+1];
-                var s2 = instructions[bumpIndex+2];
-                var s3 = instructions[bumpIndex+3];
-                var post = instructions[bumpIndex+4];
+            if (i4 == null) {
+                break;
+            }
+
+            var t1 = GetTurn(i0.Value.Dir, i1.Value.Dir);
+            var t2 = GetTurn(i1.Value.Dir, i2.Value.Dir);
+            var t3 = GetTurn(i2.Value.Dir, i3.Value.Dir);
+            var t4 = GetTurn(i3.Value.Dir, i4.Value.Dir);
+
+            var bumpS = t1.ToString() + t2.ToString() + t3.ToString();
+            Console.WriteLine($"Found {bumpS}");
+            if (bumpS == "RLL") {
+
+                var preN = i0;
+                var s1N = i1;
+                var s2N = i2;
+                var s3N = i3;
+                var postN = i4;
+
+                var pre = preN.Value;
+                var s1 = s1N.Value;
+                var s2 = s2N.Value;
+                var s3 = s3N.Value;
+                var post = postN.Value;
+
+                pre.RenderChar = '0';
+                s1.RenderChar = '1';
+                s2.RenderChar = '2';
+                s3.RenderChar = '3';
+                post.RenderChar = '4';
+
+                Render(ill);
+
+               
 
                 if (s1.Len == s3.Len) {
                     var toAdd = (s2.Len - 1) * s1.Len;
                     Console.WriteLine($"Case RLL-1 Adds {toAdd}");
                     areaAdded += toAdd;
-                    instructions.RemoveRange(bumpIndex + 1, 4);
+                    ill.Remove(s1N);
+                    ill.Remove(s2N);
+                    ill.Remove(s3N);
+                    ill.Remove(postN);
                     pre.Len += s2.Len + post.Len;
+                    illCurrent = preN.Next;
                 } else {
                     var minWidth = Math.Min(s1.Len, s3.Len);
                     var toAdd = minWidth * (s2.Len - 1);
                     areaAdded += toAdd;
 
+                    if (pre.Dir != post.Dir) {
+                        Console.WriteLine("Skipping - too soon #2");
+                        illCurrent = i1;  
+                        goto after;      
+                    }
+
                     if (s1.Len == minWidth) {
                         Console.WriteLine($"Case RLL-2 Adds {toAdd}");
-                        instructions.RemoveRange(bumpIndex + 1, 2);
+                        ill.Remove(s1N);
+                        ill.Remove(s2N);
                         pre.Len += s2.Len;
                         s3.Len -= s1.Len;
+                        illCurrent = postN;
                     } else {
                         Console.WriteLine($"Case RLL-3 Adds {toAdd}");
-                        instructions.RemoveRange(bumpIndex + 2, 2);
+                        ill.Remove(s2N);
+                        ill.Remove(s3N);
                         post.Len += s2.Len;
                         s1.Len -= s3.Len;
+                        illCurrent = postN;
                     }
                 }
-            } else if (bump.Value == "RRL") {
 
-                bumpIndex -= 1;
-                var pre = instructions[bumpIndex];
-                var s1 = instructions[bumpIndex+1];
-                var s2 = instructions[bumpIndex+2];
-                var s3 = instructions[bumpIndex+3];
-                var post = instructions[bumpIndex+4];
+                Console.WriteLine("Now:");
+                Render(ill);
+after:                
+                pre.RenderChar = (char)0;
+                s1.RenderChar = (char)0;
+                s2.RenderChar = (char)0;
+                s3.RenderChar = (char)0;
+                post.RenderChar = (char)0;
+
+            } else if (bumpS == "RRL") {
+
+               
+                var preN = i0.Previous;
+                var s1N = i1.Previous;
+                var s2N = i2.Previous;
+                var s3N = i3.Previous;
+                var postN = i4.Previous;
+
+                 if (preN == null) {
+                    Console.WriteLine("Skipping - too soon #1");
+                    illCurrent = i1;
+                    continue;
+                }
+
+                var pre = preN.Value;
+                var s1 = s1N.Value;
+                var s2 = s2N.Value;
+                var s3 = s3N.Value;
+                var post = postN.Value;
+
+                pre.RenderChar = '0';
+                s1.RenderChar = '1';
+                s2.RenderChar = '2';
+                s3.RenderChar = '3';
+                post.RenderChar = '4';
+
+                Render(ill);
+
+
+                
                 if (s1.Len == s3.Len) {
                     if (pre.Dir != post.Dir) {
-                        throw new Exception("Unhandled 1");
+                        Console.WriteLine("Skipping - too soon #3");
+                        illCurrent = i1;  
+                        goto after;      
                     }
                     var toAdd = -1 * s1.Len * (s2.Len + 1);
                     Console.WriteLine($"Case RRL-1 Adds {toAdd}");
                     areaAdded += toAdd;
-                    instructions.RemoveRange(bumpIndex + 1, 4);
+                    ill.Remove(s1N);
+                    ill.Remove(s2N);
+                    ill.Remove(s3N);
+                    ill.Remove(postN);
                     pre.Len += s2.Len + post.Len;
+                    illCurrent = preN.Next;
                 } else {
                     var minWidth = Math.Min(s1.Len, s3.Len);
                     var toAdd = -1 * minWidth * (s2.Len + 1);
                     areaAdded += toAdd;
 
                     if (s1.Len == minWidth) {
-                        Console.WriteLine($"Case RRL-2 Adds {toAdd}");
-                        instructions.RemoveRange(bumpIndex + 1, 2);
-                        pre.Len += s2.Len;
-                        s3.Len -= s1.Len;
+                        if (pre.Dir == post.Dir) {
+                            Console.WriteLine($"Case RRL-2a Adds {toAdd}");
+                            ill.Remove(s1N);
+                            ill.Remove(s2N);
+                            pre.Len += s2.Len;
+                            s3.Len -= s1.Len;
+                            illCurrent = postN;
+                        } else {
+                            Console.WriteLine($"Case RRL-2b Adds {toAdd}");
+                            ill.Remove(s1N);
+                            ill.Remove(s2N);
+                            pre.Dir = post.Dir;
+                            pre.Len = s2.Len - pre.Len;
+                            s3.Len -= s1.Len;
+                            illCurrent = postN;
+                        }
                     } else {
+                        if (pre.Dir != post.Dir) {
+                            Console.WriteLine("Skipping - too soon #4");
+                            illCurrent = i1;  
+                            goto after;      
+                        }
                         Console.WriteLine($"Case RRL-3 Adds {toAdd}");
-                        instructions.RemoveRange(bumpIndex + 2, 2);
+                        ill.Remove(s2N);
+                        ill.Remove(s3N);
                         post.Len += s2.Len;
                         s1.Len -= s3.Len;
+                        illCurrent = postN;
                     }
                 }
-            }
 
-        } else {
-            throw new Exception("Didn't find a bump");
-        }
+                Console.WriteLine("Now:");
+                Render(ill);
+after:
+                pre.RenderChar = (char)0;
+                s1.RenderChar = (char)0;
+                s2.RenderChar = (char)0;
+                s3.RenderChar = (char)0;
+                post.RenderChar = (char)0;
 
-        Render(instructions);
-/*
-        bump = turns.IndexOf("LRR");
-        if (bump >= 0) {
-
-            // Example: If bump = 1, then bump is instructions 2,3,4 and 1 and 5 are pre and post segments.
-            var pre = instructions[bump];
-            var width1 = instructions[bump+1];
-            var height = instructions[bump+2];
-            var width2 = instructions[bump+3];
-            var post = instructions[bump+4];
-
-            if (width1 == width2) {
-                areaAdded -= width1.Len * (height.Len - 1);
-                instructions.RemoveRange(bump + 1, 4);
-                pre.Len += height.Len + post.Len;
             } else {
-                var minWidth = Math.Min(width1.Len, width2.Len);
-                areaAdded -= minWidth * (height.Len -1);
-
-                if (width1.Len == minWidth) {
-                    instructions.RemoveRange(bump + 1, 2);
-                    pre.Len += height.Len;
-                    width2.Len -= (width2.Len - width1.Len);
-                } else {
-                    instructions.RemoveRange(bump + 2, 2);
-                    post.Len += height.Len;
-                    width1.Len -= (width1.Len - width2.Len);
-                }
+                illCurrent = illCurrent.Next;
             }
+            
 
-            continue;
         }
-*/
-
+ 
 
     }
 
-    if (instructions[0].Len != instructions[2].Len || instructions[1].Len != instructions[3].Len) {
+    var i0F = ill.First;
+    var i1F = i0F?.Next;
+    var i2F = i1F?.Next;
+    var i3F = i2F?.Next;
+
+    if (i0F.Value.Len != i2F.Value.Len || i1F.Value.Len != i3F.Value.Len) {
         throw new Exception("Didn't find a rectangle");
     }
 
-    var area = (instructions[0].Len + 1) * (instructions[1].Len + 1);
+    var area = (i0F.Value.Len + 1) * (i1F.Value.Len + 1);
 
     area -= areaAdded;
 
@@ -185,54 +273,90 @@ void Part2(string[] lines)
 
 }
 
-void Render(List<Instruction> instructions) {
 
-    int boardWidth = 500;//400;
-    int boardHeight = 500;// 250;
 
-    int originWidth = 200;//110;
-    int originHeight = 300;//200;
-    var map = new char[boardHeight, boardWidth];
+void Render(IEnumerable<Instruction> instructions, bool big = false) {
 
+    if (RenderCount++ < RenderSkip) {
+        return;
+    }
+
+    if (RenderCount > RenderThrow) {
+        throw new Exception("Render Throw for debugging");
+    }
+
+    int boardWidth = 400;
+    int boardHeight = 250;
+
+    int originWidth = 110;
+    int originHeight = 200;
+
+    if (big) {
+        boardWidth = 600;
+        boardHeight = 600;
+        originHeight = 300;
+        originWidth = 300;
+    }
+
+    if (sample)
+    {
+        boardWidth = 15;
+        boardHeight = 15;
+        originHeight = 0;
+        originWidth = 0;
+    }
+    var map = new string[boardHeight, boardWidth];
     long curRow = originHeight;
     long curCol = originWidth;
-    char c = '#';
-    foreach(var instruction in instructions) {
-        if (instruction.Dir == Dir.D) {
-            for (int deltaR = 0; deltaR < instruction.Len; deltaR++) {
-                map[curRow + deltaR, curCol] = c;
-            }
-            curRow += instruction.Len;
-        } else if (instruction.Dir == Dir.U) {
-            for (int deltaR = 0; deltaR < instruction.Len; deltaR++) {
-                map[curRow - deltaR, curCol] = c;
-            }
-            curRow -= instruction.Len;
-        } else if (instruction.Dir == Dir.R) {
-            for (int deltaC = 0; deltaC < instruction.Len; deltaC++) {
-                map[curRow, curCol  + deltaC] = c;
-            }
-            curCol += instruction.Len;
-        } else if (instruction.Dir == Dir.L) {
-            for (int deltaC = 0; deltaC < instruction.Len; deltaC++) {
-                map[curRow, curCol - deltaC] = c;
-            }
-            curCol -= instruction.Len;
-        }
+    try {
+        
+        
+        foreach(var instruction in instructions) {
 
-        //c++;
+            var c = instruction.RenderChar == 0 ? "#" : $"{RED}{instruction.RenderChar}{NORMAL}";
+            if (instruction.Dir == Dir.D) {
+                for (int deltaR = 0; deltaR < instruction.Len; deltaR++) {
+                    map[curRow + deltaR, curCol] = c;
+                }
+                curRow += instruction.Len;
+            } else if (instruction.Dir == Dir.U) {
+                for (int deltaR = 0; deltaR < instruction.Len; deltaR++) {
+                    map[curRow - deltaR, curCol] = c;
+                }
+                curRow -= instruction.Len;
+            } else if (instruction.Dir == Dir.R) {
+                for (int deltaC = 0; deltaC < instruction.Len; deltaC++) {
+                    map[curRow, curCol  + deltaC] = c;
+                }
+                curCol += instruction.Len;
+            } else if (instruction.Dir == Dir.L) {
+                for (int deltaC = 0; deltaC < instruction.Len; deltaC++) {
+                    map[curRow, curCol - deltaC] = c;
+                }
+                curCol -= instruction.Len;
+            }
+
+        }
+    } catch (Exception) {
+        if (!big) {
+            Render(instructions, true);
+        }
+        else {
+            throw;
+        }
     }
 
     StringBuilder sb = new();
+    sb.AppendLine();
     for (int row = 0; row < boardHeight; row++) {
         for (int col = 0; col < boardWidth; col++) {
 
             bool origin = row == originHeight && col == originWidth;
             if (origin) {
-                sb.Append(RED);
+                sb.Append(GREEN);
             }
             var ch = map[row,col];
-            sb.Append(ch == 0 ? ' ' : ch );
+            sb.Append(ch == null ? ' ' : ch );
             if (origin) {
                 sb.Append(NORMAL);
             }
@@ -273,6 +397,7 @@ Turn GetTurn(Dir d1, Dir d2) {
 record Instruction {
     public Dir Dir;
     public long Len;
+    public char RenderChar;
 }
 
 enum Dir {
