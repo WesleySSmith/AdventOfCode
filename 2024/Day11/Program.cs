c92e09a1d2ddf26e27b7bf0217525ee6a2b81af2
Wes Smith <wesleysmith@yahoo.com>
2024-12-22 21:54:05 -0800

    2024 Day 11, 12

---
+++ 2024/Day11/Program.cs
@@ -0,0 +1,95 @@
+﻿#pragma warning disable CS8524
+#pragma warning disable CS8509
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using MoreLinq;
+//using MoreLinq;
+
+bool sample = false;
+
+string[] lines = File.ReadAllLines(sample ? "sample.txt" : "input.txt");
+//Console.Out.WriteLine($"Read {lines.Length} lines from {lines.First()} to {lines.Last()}");
+
+Stopwatch sw = Stopwatch.StartNew();
+
+//Part1(lines);
+Part2(lines);
+
+Console.Out.WriteLine($"Finished in {sw.ElapsedMilliseconds}ms");
+
+
+// 1234
+
+
+void Part1(string[] lines)
+{
+    var stones = lines[0].Split(' ').Select(long.Parse).ToList();
+
+    for (int l = 0; l < 25; l++) {
+        Console.Out.WriteLine(l);
+        var len = stones.Count;
+        for (int i = 0; i < len; i++) {
+
+            var stone = stones[i];
+            if (stone == 0) {
+                stones[i] = 1;
+            } else {
+                int numDigits = (int)Math.Floor(Math.Log10(stone) + 1);
+                if (numDigits % 2 == 0) {
+                    var leftHalf = (long)(stone / Math.Pow(10, numDigits / 2));
+                    var rightHalf = (long)(stone - leftHalf * Math.Pow(10, numDigits / 2));
+                    stones[i] = leftHalf;
+                    stones.Add(rightHalf);
+                } else {
+                    stones[i] = stone * 2024;
+                }
+            }
+        }
+    }
+
+
+    Console.Out.WriteLine($"Part 1: {stones.Count()}");
+}
+
+
+void Part2(string[] lines) {
+    var stones = lines[0].Split(' ').Select(long.Parse).ToList();
+    var stonesD = stones.ToDictionary(s => s, s => 1L);
+
+    
+    for (int l = 0; l < 75; l++) {
+        var stonesD2 = new Dictionary<long, long>(stones.Count * 2);
+        //Console.Out.WriteLine(l);
+        foreach (var stoneKV in stonesD) {
+            var stone = stoneKV.Key;
+            var count = stoneKV.Value;
+            if (stone == 0) {
+                stonesD2[1] = count + (stonesD2.TryGetValue(1, out var existing) ? existing : 0L);
+                //CollectionsMarshal.GetValueRefOrAddDefault(stonesD2, 1, out _) += count;
+            } else {
+                int numDigits = (int)Math.Floor(Math.Log10(stone) + 1);
+                if (numDigits % 2 == 0) {
+                    var leftHalf = (long)(stone / Math.Pow(10, numDigits / 2));
+                    var rightHalf = (long)(stone - leftHalf * Math.Pow(10, numDigits / 2));
+
+                    stonesD2[leftHalf] = count + (stonesD2.TryGetValue(leftHalf, out var existing) ? existing : 0L);
+                    stonesD2[rightHalf] = count + (stonesD2.TryGetValue(rightHalf, out var existing2) ? existing2 : 0L);
+                    //CollectionsMarshal.GetValueRefOrAddDefault(stonesD2, leftHalf, out _) += count;
+                    //CollectionsMarshal.GetValueRefOrAddDefault(stonesD2, rightHalf, out _) += count;
+                } else {
+                    var newVal = stone * 2024;
+                    stonesD2[newVal] = count + (stonesD2.TryGetValue(newVal, out var existing) ? existing : 0L);
+                    //CollectionsMarshal.GetValueRefOrAddDefault(stonesD2, newVal, out _) += count;
+                }
+            }
+        }
+        stonesD = stonesD2;
+    }
+
+    var totalStones = stonesD.Sum(x => x.Value);
+    Console.Out.WriteLine($"Part 2: {totalStones}");
+}
+
---
+++ 2024/Day12/Program.cs
@@ -0,0 +1,281 @@
+﻿#pragma warning disable CS8524
+#pragma warning disable CS8509
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using MoreLinq;
+//using MoreLinq;
+
+bool sample = false;
+
+string[] lines = File.ReadAllLines(sample ? "sample.txt" : "input.txt");
+//Console.Out.WriteLine($"Read {lines.Length} lines from {lines.First()} to {lines.Last()}");
+
+Stopwatch sw = Stopwatch.StartNew();
+
+//Part1(lines);
+Part2(lines);
+
+Console.Out.WriteLine($"Finished in {sw.ElapsedMilliseconds}ms");
+
+
+
+void Part1(string[] lines)
+{
+    var rows = lines.Length;
+    var cols = lines[0].Length;
+
+    var map = new (char Plant, bool Visited)[rows+2,cols+2];
+
+    for (int r = 0; r < rows +2; r++) {
+        for (int c = 0; c < cols +2; c++) {
+            if (r == 0 || c == 0 || r == rows+1 || c == cols+1) {
+                map[r,c] = ((char)0, false);
+            }
+            else {
+                map[r,c] = (lines[r-1][c-1], false);
+            }
+        }
+    } 
+
+    List<(int Area, int Perim)> bigList = new ();
+    for (int i = 1; i < rows + 1; i++) {
+        for (int j = 1; j < cols +1; j++) {
+
+            if (map[i,j].Plant == 0 || map[i,j].Visited) {
+                continue;
+            }
+
+            int area = 0;
+            int perim = 0;
+            Stack<RC> stack = new();
+            var current = new  RC(i, j);
+            area++;
+            stack.Push(current);
+            var plant = map[current.Row, current.Col].Plant;
+            map[current.Row,current.Col].Visited = true;
+            while (stack.TryPop(out current)) {
+                var left = current with {Row = current.Row -1};
+                if (map[left.Row,left.Col].Plant == plant) {
+                    if (!map[left.Row,left.Col].Visited) {
+                        stack.Push(left);
+                        area++;
+                        map[left.Row,left.Col].Visited = true;
+                    }
+                } else {
+                    perim++;
+                }
+                var right = current with {Row = current.Row +1};
+                if (map[right.Row,right.Col].Plant == plant) {
+                    if (!map[right.Row,right.Col].Visited) {
+                        stack.Push(right);
+                        area++;
+                        map[right.Row,right.Col].Visited = true;
+                    }
+                } else {
+                    perim++;
+                }
+                var up = current with {Col = current.Col -1};
+                if (map[up.Row,up.Col].Plant == plant) {
+                    if (!map[up.Row,up.Col].Visited) {
+                        stack.Push(up);
+                        area++;
+                        map[up.Row,up.Col].Visited = true;
+                    }
+                }
+                else {
+                    perim++;
+                }
+                var down = current with {Col = current.Col +1};
+                if (map[down.Row,down.Col] .Plant == plant) {
+                    if (!map[down.Row,down.Col].Visited) {
+                        stack.Push(down);
+                        area++;
+                        map[down.Row,down.Col].Visited = true;
+                    }
+                } else {
+                    perim++;
+                }
+            }
+            bigList.Add((area, perim));
+        }
+    }
+
+
+    var score = bigList.Sum(l => l.Area * l.Perim);
+
+    Console.Out.WriteLine(score);
+
+
+    //Console.Out.WriteLine($"Part 1: {stones.Count()}");
+}
+
+
+void Part2(string[] lines) {
+    var rows = lines.Length;
+    var cols = lines[0].Length;
+
+    var map = new (char Plant, bool Visited)[rows+2,cols+2];
+
+    for (int r = 0; r < rows +2; r++) {
+        for (int c = 0; c < cols +2; c++) {
+            if (r == 0 || c == 0 || r == rows+1 || c == cols+1) {
+                map[r,c] = ((char)0, false);
+            }
+            else {
+                map[r,c] = (lines[r-1][c-1], false);
+            }
+        }
+    } 
+    List<Lelem> bigList = new ();
+    Dictionary<RC, Lelem> bigMap = new();
+    for (int i = 1; i < rows + 1; i++) {
+        for (int j = 1; j < cols +1; j++) {
+
+            if (map[i,j].Plant == 0 || map[i,j].Visited) {
+                continue;
+            }
+
+            Stack<RC> stack = new();
+            var current = new  RC(i, j);
+            stack.Push(current);
+            var plant = map[current.Row, current.Col].Plant;
+            map[current.Row,current.Col].Visited = true;
+            Lelem lelem = new Lelem{ Area = 1, Plant = plant};
+            bigMap[current] = lelem;
+
+            while (stack.TryPop(out current)) {
+                var left = current with {Row = current.Row -1};
+                if (map[left.Row,left.Col].Plant == plant) {
+                    if (!map[left.Row,left.Col].Visited) {
+                        stack.Push(left);
+                        lelem.Area++;
+                        map[left.Row,left.Col].Visited = true;
+                        bigMap[left] = lelem;
+                    }
+                } else {
+                    lelem.Perimeter++;
+                }
+                var right = current with {Row = current.Row +1};
+                if (map[right.Row,right.Col].Plant == plant) {
+                    if (!map[right.Row,right.Col].Visited) {
+                        stack.Push(right);
+                        lelem.Area++;
+                        map[right.Row,right.Col].Visited = true;
+                        bigMap[right] = lelem;
+                    }
+                } else {
+                    lelem.Perimeter++;
+                }
+                var up = current with {Col = current.Col -1};
+                if (map[up.Row,up.Col].Plant == plant) {
+                    if (!map[up.Row,up.Col].Visited) {
+                        stack.Push(up);
+                        lelem.Area++;
+                        map[up.Row,up.Col].Visited = true;
+                        bigMap[up] = lelem;
+                    }
+                }
+                else {
+                    lelem.Perimeter++;
+                }
+                var down = current with {Col = current.Col +1};
+                if (map[down.Row,down.Col] .Plant == plant) {
+                    if (!map[down.Row,down.Col].Visited) {
+                        stack.Push(down);
+                        lelem.Area++;
+                        map[down.Row,down.Col].Visited = true;
+                        bigMap[down] = lelem;
+                    }
+                } else {
+                    lelem.Perimeter++;
+                }
+            }
+            bigList.Add(lelem);
+        }
+    }
+
+    for (int i = 1; i < rows + 1; i++) {
+        for (int j = 1; j < cols +1; j++) {
+
+            var current = new  RC(i, j);
+            var plant = map[current.Row, current.Col].Plant;
+
+
+            var up = current with {Row = current.Row - 1};
+            var down = current with {Row = current.Row + 1};
+            var right = current with {Col = current.Col + 1};
+            var upright = current with {Row = current.Row - 1, Col = current.Col + 1};
+            var downright = current with {Row = current.Row + 1, Col = current.Col + 1};
+                
+            if (map[up.Row,up.Col].Plant != plant) {
+                // has a fence on the top
+                if (map[right.Row,right.Col].Plant != plant || map[upright.Row, upright.Col].Plant == plant) {
+                    // The top fence is ending, so count it
+                    bigMap[current].Sides++;
+                }
+            }
+
+            if (map[down.Row,down.Col] .Plant != plant) {
+                // has a fence on the bottom
+                if (map[right.Row,right.Col].Plant != plant || map[downright.Row, downright.Col].Plant == plant) {
+                    // The bottom fence is ending, so count it
+                    bigMap[current].Sides++;
+                }
+            }
+        }
+    }
+
+    for (int j = 1; j < cols +1; j++) {
+        for (int i = 1; i < rows + 1; i++) {
+
+            var current = new  RC(i, j);
+            var plant = map[current.Row, current.Col].Plant;
+
+            var up = current with {Row = current.Row - 1};
+            var down = current with {Row = current.Row + 1};
+            var right = current with {Col = current.Col + 1};
+            var left = current with {Col = current.Col - 1};
+            var upright = current with {Row = current.Row - 1, Col = current.Col + 1};
+            var downright = current with {Row = current.Row + 1, Col = current.Col + 1};
+            var downleft = current with {Row = current.Row +1, Col = current.Col - 1};
+                
+            if (map[right.Row,right.Col].Plant != plant) {
+                // has a fence on the right
+                if (map[down.Row,down.Col].Plant != plant || map[downright.Row, downright.Col].Plant == plant) {
+                    // The right fence is ending, so count it
+                    bigMap[current].Sides++;
+                }
+            }
+
+            if (map[left.Row,left.Col] .Plant != plant) {
+                // has a fence on the left
+                if (map[down.Row,down.Col].Plant != plant || map[downleft.Row, downleft.Col].Plant == plant) {
+                    // The left fence is ending, so count it
+                    bigMap[current].Sides++;
+                }
+            }
+        }
+    }
+
+
+
+    var score = bigList.Sum(l => l.Area * l.Sides);
+
+    Console.Out.WriteLine(score);
+
+
+   
+}
+
+record RC(int Row, int Col);
+record Lelem() {
+    public int Area {get; set;}
+    public char Plant {get; init;}
+    public int Perimeter {get; set;}
+
+    public int Sides {get; set;}
+}
+
